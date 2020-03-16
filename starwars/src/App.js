37aaf342ba4dccf5e8043c52cd584ea0bebf8adb
AustinLynes <austinlynes94@gmail.com>
2020-03-16 09:15:52 -0700

    added stretch for TL stuff

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useImperativeHandle } from 'react';
 import axios from 'axios';
 import './App.css';
 import CharacterCard from './components/CharacterCard';
@@ -10,19 +10,29 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   const [people, setPeople] = useState([]);
+  let [page_count, setPageCount] = useState(1)
 
   useEffect(() => {
     axios
-      .get('https://swapi.co/api/people/')
+      .get(`https://swapi.co/api/people/?page=${page_count}`)
       .then(res => setPeople(res.data.results))
-      .catch(err => err);
-  }, [people]);
+      .catch(err => console.log(err));
+  }, [page_count]);
 
+
+  const handleClick = (str) => {
+    str === 'next' ?
+      setPageCount(page_count += 1)
+      :
+      setPageCount(page_count -= 1)
+    if (page_count < 0 || page_count > 8)
+      setPageCount(1)
+  }
 
   function CreateCharacterCard(person) {
     return (
       <CharacterCard
-         key={person.name}
+        key={person.name}
         name={person.name}
         dob={person.birth_year}
         height={person.height}
@@ -44,6 +54,14 @@ const App = () => {
           })}
         </Card.Group>
       </Container>
+      <div style={{display:'flex',flexFlow:'row wrap', width:'80%', justifyContent:'space-between', alignItems:"center",alignContent:"center",margin:'0 auto', padding:'1rem'}}>
+
+        <p style={{ color: 'white',background:'black', borderRadius:'50%', padding:'.5rem 1rem', fontSize: '1.4rem' }}>{page_count}</p>
+        {page_count > 1 ?
+          <button onClick={() => handleClick('prev')}>previous</button>
+          : ''}
+        <button onClick={() => handleClick('next')}>next</button>
+      </div>
     </div>
   );
 };
